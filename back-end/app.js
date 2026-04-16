c7794dd993ad099d1b6892d4b4cc3ed88c5e6448
HB <670486183@qq.com>
2026-04-16 21:39:48 +0800

    添加 CORS 支持

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
